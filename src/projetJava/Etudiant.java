87faddf9b63a69add8414545162ec0b9cddb01ba
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2019-11-08 10:23:08 +0100

    test 1

---
+++ src/projetJava/Etudiant.java
@@ -0,0 +1,12 @@
+package projetJava;
+
+public class Etudiant {
+	private String nom;
+	private String prenom;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
