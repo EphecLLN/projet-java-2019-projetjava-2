5ce7b01929c0a050ff3067807784a66c61de4a10
Victor C <vict918@gmail.com>
2019-11-08 14:47:55 +0100

    Réorganisation des fichiers

---
+++ Projet/src/projetJava/Tache.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package projetJava;
+
+/**
+ * @author robin
+ *
+ */
+public class Tache {
+	
+	private int idTache;
+	private String nomTache;
+	private Date dateLimite;
+	private static String [] allTaches;
+	
+
+}
