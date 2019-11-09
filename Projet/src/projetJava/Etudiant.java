15736a66c6103753a7c8f69b3ad2a6d2446d5460
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2019-11-09 14:52:31 +0100

    constructeurs

--- Projet/src/projetJava/Etudiant.java
+++ Projet/src/projetJava/Etudiant.java
@@ -5,25 +5,129 @@ public class Etudiant {
 	private String prenom;
 	private int idEtu;
 	private Kot infoKot;
+	private static int nbEtudiants;
 	
+	
+	//--------------------------------------CONSTRUCTEURS-------------------------------------------
 	public Etudiant(String nom, String prenom, Kot infoKot) {
 		this.nom = nom;
 		this.prenom = prenom;
-		//this.idEtu géré avec un compteur 
 		this.infoKot = infoKot;
+		
+		nbEtudiants++;
+		
+		//initialiser l'id de l'étudiant
+		this.idEtu = nbEtudiants;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Etudiant etu1 = new Etudiant("Castermane", "Robin", new Kot("kot à projet"));
-		Etudiant etu2 = new Etudiant("Cotton", "Victor", new Kot("kot à projet"));
-
-		for(int i = 0; i < etu1.infoKot.allKot.length; i++) {
-			System.out.println(etu1.infoKot.allKot[i]);
-		}
-			
-		
+	//------------------------------------GETTERS & SETTERS-----------------------------------------
+	
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
 	}
 
+
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+
+	/**
+	 * @return the idEtu
+	 */
+	public int getIdEtu() {
+		return idEtu;
+	}
+
+
+
+	/**
+	 * @param idEtu the idEtu to set
+	 */
+	public void setIdEtu(int idEtu) {
+		this.idEtu = idEtu;
+	}
+
+
+
+	/**
+	 * @return the infoKot
+	 */
+	public Kot getInfoKot() {
+		return infoKot;
+	}
+
+
+
+	/**
+	 * @param infoKot the infoKot to set
+	 */
+	public void setInfoKot(Kot infoKot) {
+		this.infoKot = infoKot;
+	}
+
+
+
+	/**
+	 * @return the nbEtudiants
+	 */
+	public static int getNbEtudiants() {
+		return nbEtudiants;
+	}
+
+
+
+	/**
+	 * @param nbEtudiants the nbEtudiants to set
+	 */
+	public static void setNbEtudiants(int nbEtudiants) {
+		Etudiant.nbEtudiants = nbEtudiants;
+	}
+
+
+	//-----------------------------------------MAIN----------------------------------------------
+	public static void main(String[] args) {
+		
+		Etudiant etu1 = new Etudiant("Castermane", "Robin", new Kot("kot à projet"));
+		Etudiant etu2 = new Etudiant("Cotton", "Victor", new Kot(1));
+		Etudiant etu3 = new Etudiant("Castermane", "Florent", new Kot("les shrabs"));
+		Etudiant etu4 = new Etudiant("Du Jardin", "Alex", new Kot("Familly"));
+		Etudiant etu5 = new Etudiant("Zuccet", "Alexandra", new Kot(2));
+		Etudiant etu6 = new Etudiant("Du riz", "Deborah", new Kot(1));
+
+		for(int i = 0; i < etu1.infoKot.allKot.length; i++) {
+			if(etu1.infoKot.allKot[i][0] == null) {
+				break;
+			}
+			System.out.print("Nom du premier Kot : '" +etu1.infoKot.allKot[i][0]);
+			System.out.println("' et son Id : " + etu1.infoKot.allKot[i][1]);
+		}
+		System.out.println(etu5.idEtu);
+	}
 }
--- Projet/src/projetJava/Kot.java
+++ Projet/src/projetJava/Kot.java
@@ -6,30 +6,44 @@ package projetJava;
  */
 public class Kot {
 
-		public String nomKot;
-		private int idKot;
-		public static String [] allKot = new String[100];
-		public static int nombreDeKot;
+	public String nomKot;
+	private int idKot;
+	public static String [][] allKot = new String[100][2];
+	public static int nombreDeKot;
+	
+	
+	/**
+	 * Constructeur qui créé un nouveau kot sur base de son nom
+	 * @param nomKot
+	 */
+	public Kot(String nomKot) { // si l'étudiant n'a pas de kot il en crée un (donc nouveau nom)
+		this.nomKot = nomKot;
 		
-		public Kot(String nomKot) {
-			this.nomKot = nomKot;
-			
-			nombreDeKot ++;
-			this.allKot[nombreDeKot - 1] = nomKot;
-			
-			
-			for(int i = 0; i < this.allKot.length; i++)	{
-				if(this.nomKot == this.allKot[i]) {
-					this.allKot[i] = null;
-					nombreDeKot --;
-				}
-			}		
-		}
+		// met les noms des kot dans le tableau all kot à l'indice 0
+		this.allKot[nombreDeKot][0] = nomKot; 
 		
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-
-			
-			
-		}
+		nombreDeKot ++; 
+		
+		//initialise l'id du kot
+		this.idKot = nombreDeKot; 
+		
+		// met une id (qui représente le numéro de kot) dans le tableau all kot à l'indice 1
+		this.allKot[nombreDeKot-1][1] = String.valueOf(nombreDeKot);
+	}
+	
+	
+	
+	
+	/**
+	 * Constructeur qui ajoute l'étudiant au kot dont l'id est mise en paramètre
+	 * @param idKot
+	 */
+	public Kot(int idKot) {
+		//parcourt le tableau de tout les kot, et vérifie si ils ont été créé ( != null )
+		for(int i = 0; i < this.allKot.length && this.allKot[i][0] != null; i++) {
+			if(idKot == Integer.parseInt(this.allKot[i][1])) {
+				this.nomKot = this.allKot[i][0];
+			}
+		}	
+	}
 }
