82269ffebaa4dcb8094a0c65f8af42c0f57de212
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2019-11-12 21:33:36 +0100

    Test nouvelle class

---
+++ Projet/src/projetJava/Task.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package projetJava;
+
+import java.util.Date;
+/**
+ * @author robin
+ *
+ */
+public class Task {
+
+	private long id;
+	private String taskName;
+	private Student Student;
+	private Date deadline;
+	
+	public Task(String taskName) {
+		this.taskName = taskName;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
